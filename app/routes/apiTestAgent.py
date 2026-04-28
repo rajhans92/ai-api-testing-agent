c2d362f3a61e0aa1a12feaff0d4341563274ebd2
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-04-28 23:56:35 +0530

    create route file

--- app/routes/apiTestAgent.py
+++ app/routes/apiTestAgent.py
@@ -1,15 +1,20 @@
-from fastapi import APIRouter
-from app.models.apiParser import (
-    SwaggerDocument,
-    API,        
-    APIParameter,
-    APIResponse,
-    APIAuth,
-    APIDependency,
-    ParameterMapping
+from fastapi import APIRouter, HTTPException, Depends
+from app.schemas.apiParser import (
+    ProjectDetails,
 )
+from app.services.handler import (
+    get_api_parser_service,
+)
+from app.services.apiParser import APIParserService
+
 router = APIRouter(prefix="/api-test-agent", tags=["api-test-agent"])
 
-@router.get("/test")
-async def test_endpoint():
-    return {"message": "API Testing Agent is working!"}
+@router.post("/project")
+async def create_project(project: ProjectDetails, apiParseService: APIParserService = Depends(get_api_parser_service)):
+    try:
+        project_id = await apiParseService.create_project(project)
+        return {"message": "Project created successfully", "project_id": project_id}
+    except Exception as e:
+        print("Error creating project: ", str(e))
+        raise HTTPException(status_code=500, detail="Failed to create project")  
+    
