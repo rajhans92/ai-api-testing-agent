9b9b3dea7b52e76b72026832a273b94a4ffc29f4
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-05-16 14:50:23 +0530

    update parse code

--- app/agents/apiParserAgent.py
+++ app/agents/apiParserAgent.py
@@ -1,8 +1,8 @@
 from langgraph.graph import StateGraph, START, END
 from langchain.chat_models import init_chat_model
-from langchain_core.prompts import PromptTemplate
 from typing_extensions import TypedDict
 from sqlalchemy.orm import Session
+from sqlalchemy import select,String
 from app.utils.config import (
     LLM_MODEL,
     OPENAI_API_KEY
@@ -19,89 +19,106 @@ llm = init_chat_model(model=LLM_MODEL, api_key=OPENAI_API_KEY)
 
 # Graph state
 class State(TypedDict):
-    swaggerId: str
+    swaggerId: int
     db: Session
 
 
-def createDependencyGraph(state: State):
+async def createDependencyGraph(state: State):
         swaggerId = state["swaggerId"]
         db = state["db"]
         # Fetch API details from database using swaggerId, use LLM to create dependency graph,
-        apiList = (db.query(API, APIParameter)
-                    .join(
-                        APIParameter,
-                        API.id == APIParameter.api_id
-                    )
-                    .filter(
-                        API.swagger_id == swaggerId
-                    )
-                    .all())
+        print(f"Fetching API details for swaggerId: {swaggerId}")
+        stmt = (
+            select(API, APIParameter)
+            .join(
+                APIParameter,
+                API.id == APIParameter.api_id
+            )
+            .where(
+                API.swagger_id == swaggerId
+            )
+        )
+
+        result = await db.execute(stmt)
+
+        apiList = result.all()
+        print(f"Fetched {len(apiList)} APIs for swaggerId: {swaggerId}")
         sourceApiDetiails= {}
         setOfDependency = {}
-        for api in apiList:
+        run = 0
+        for api_obj, param_obj in apiList:
+            if(run >= 5):
+                break
+            run += 1
             sourceApiDetiails = {
-                "sourceApiId" : api.id,
-                "unique_path" : api.unique_path,
-                "method" : api.method,
-                "path" : api.path,
-                "summary" : api.summary,
-                "description" : api.description,
-                "requestBody" : api.row_json,
-                "location" : api.location,
-                "required" : api.required
+                "sourceApiId": api_obj.id,
+                "unique_path": api_obj.unique_path,
+                "method": api_obj.method,
+                "path": api_obj.path,
+                "summary": api_obj.summary,
+                "description": api_obj.description,
+                "requestBody": param_obj.schema,
+                "location": param_obj.location,
+                "required": param_obj.required
             }
             print(f"Source API Details: {sourceApiDetiails}")
             print("--------------------------------------------------")
             print("---------------------------------------------------")
-            for key, value in sourceApiDetiails["requestBody"]:
-                print(f"Source API requestBody: {key} : {value}")
+            for key, value in sourceApiDetiails["requestBody"].items():
+                # print(f"Source API requestBody: {key} : {value}")
+                # print("--------------------------------------------------")
+                # print("---------------------------------------------------")
+                
+                stmt = (
+                    select(API, APIResponse)
+                    .join(
+                        APIResponse,
+                        API.id == APIResponse.api_id
+                    )
+                    .filter(
+                        API.swagger_id == swaggerId,
+                        APIResponse.schema.cast(String).ilike(f"%{key}%")
+                    )
+                )
+
+                result = await db.execute(stmt)
+
+                requestDetailApi = result.all()
+
+                for api_depend_obj, response_obj in requestDetailApi:
+                    setOfDependency[api_depend_obj.unique_path] = {
+                        "id": api_depend_obj.id,
+                        "method": api_depend_obj.method,
+                        "path": api_depend_obj.path,
+                        "summary": api_depend_obj.summary,
+                        "description": api_depend_obj.description,
+                        "response_schema": response_obj.schema
+                    }
+                    # print(f"Dependent API Details: {setOfDependency[api_depend_obj.unique_path]}")
+                    print(f" unique_path = {api_depend_obj.unique_path}")
+                print (f" Count = {len(setOfDependency)}")
                 print("--------------------------------------------------")
                 print("---------------------------------------------------")
-                requestDetailApi = (
-                                    db.query(API, APIResponse)
-                                    .join(
-                                        APIResponse,
-                                        API.id == APIResponse.api_id
-                                    )
-                                    .filter(
-                                        API.swagger_id == swaggerId,
-                                        API.schema.ilike(f"%{key}%")
-                                    )
-                                    .all()
-                                )
-                for apiDepend in requestDetailApi:
-                    setOfDependency[apiDepend.unique_path] = {
-                            "id": apiDepend.id,
-                            "method": apiDepend.method,
-                            "path": apiDepend.path,
-                            "summary": apiDepend.summary,
-                            "description": apiDepend.description,
-                            "response_schema": apiDepend.schema
-
-                    }
-                    print(f"Dependent API Details: {setOfDependency[apiDepend.unique_path]}")
-                    print("--------------------------------------------------")
-                    print("---------------------------------------------------")
             prompt = loadPrompt("dependencyPrompt.txt", sourceApiDetails=sourceApiDetiails, setOfDependency=setOfDependency)
-            chain = llm | prompt
-            response = chain.invoke()
+            print(f"Prompt for API {api_obj.id}: {prompt}")
+            response = await llm.ainvoke(prompt)
 
-            print(f"Dependency graph for API {api.id}: {response}")
+            print(f"Dependency graph for API {api_obj.id}")
+            print(response.content)
             print("--------------------------------------------------")
             print("--------------------------------------------------")
             print("--------------------------------------------------")
-            break
                   
 
-def executeApiAndTest(state: State):
+async def executeApiAndTest(state: State):
     # Execute APIs based on dependency graph and run tests, update state with results
     pass
 
-def failerHandler(state: State):
+async def failerHandler(state: State):
     # Analyze failed tests and update state with failure details
     pass
 
-def reevaluateDependencyGraph(state: State):
+async def reevaluateDependencyGraph(state: State):
     # Use LLM to analyze failure details and suggest updates to dependency graph, update state
     pass
 
@@ -115,16 +132,22 @@ apiParserGraph.add_node("failerHandler",failerHandler)
 
 # Add edges to connect nodes
 apiParserGraph.add_edge(START, "createDependencyGraph")
-apiParserGraph.add_edge("createDependencyGraph", "executeApiAndTest")
-apiParserGraph.add_edge("executeApiAndTest", "failerHandler")
-apiParserGraph.add_edge("failerHandler", "reevaluateDependencyGraph")
-apiParserGraph.add_edge("failerHandler", "executeApiAndTest")
-apiParserGraph.add_edge("reevaluateDependencyGraph", "executeApiAndTest")
-apiParserGraph.add_edge("executeApiAndTest", END)
+apiParserGraph.add_edge("createDependencyGraph", END)
+
+# apiParserGraph.add_edge("createDependencyGraph", "executeApiAndTest")
+# apiParserGraph.add_edge("executeApiAndTest", "failerHandler")
+# apiParserGraph.add_edge("failerHandler", "reevaluateDependencyGraph")
+# apiParserGraph.add_edge("failerHandler", "executeApiAndTest")
+# apiParserGraph.add_edge("reevaluateDependencyGraph", "executeApiAndTest")
+# apiParserGraph.add_edge("executeApiAndTest", END)
 
 apiParserAgent = apiParserGraph.compile()
 
 async def executeAPIParserAgent(swaggerId: int, db: Session):
-        print(f"Executing API Parser Agent with swaggerId: {swaggerId}")
-        await apiParserAgent.invoke({swaggerId: swaggerId, db:db})
-        print("API Parser Agent execution completed.")
+        try:
+            print(f"Executing API Parser Agent with swaggerId: {swaggerId}")
+            await apiParserAgent.ainvoke({"swaggerId": swaggerId, "db":db})
+            print("API Parser Agent execution completed.")
+        except Exception as e:
+            print(e)
+
